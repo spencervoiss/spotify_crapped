64e7ebcc140447807fd47d1e74653322f71f8c7d
Spencer Voiss <spenceravoiss@gmail.com>
2024-12-18 15:25:01 -0600

    formatting

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@ setuptools.setup(
     name="spotify_crapped",
     version="0.1.0",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "pandas",
-        "pywebview"
-    ],
+    install_requires=["pandas", "pywebview"],
     entry_points={
         "console_scripts": [
             "spotify_crapped = spotify_crapped.main:main",
